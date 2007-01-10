43a6aeeeebb6c7341b699e1cf3d314588ae7cf49
Stephen Yeargin <stephen@yearg.in>
2007-01-10 01:48:54 +0000

    Better file handling for systems that don't start with 'Mac OS X'

--- includes/config-sample.php
+++ includes/config-sample.php
@@ -1,9 +1,9 @@
 <?php
 /* Database and Template Settings */
 define("DB_HOSTNAME", "localhost");
-define("DB_DATABASE", "pacercms");
-define("DB_USERNAME", "username");
-define("DB_PASSWORD", "password");
+define("DB_DATABASE", "pacer");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
 define("TEMPLATES_FOLDER", "default");
 
 
@@ -16,8 +16,9 @@ define("COMPILE_CHECK", true);
 /* Below this line should work without customization */
 $cm_path = substr(dirname(__FILE__),0,-9); // removes '/includes/'
 define("SITE_ROOT", $cm_path);
-define("SMARTY_DIR", $cm_path . "/includes/smarty/");
+define("ADODB_DIR", $cm_path . "/includes/adodb_lite/");
+define("SMARTY_DIR", $cm_path . "/includes/Smarty/");
 define("TEMPLATES_PATH", $cm_path . "/templates/" . TEMPLATES_FOLDER);
 define("TEMPLATES_C_PATH", $cm_path . "/cache/");
 define("CACHE_PATH", $cm_path . "/cache/");
-define("CONFIGS_PATH", SMARTY_DIR . "configs/");
+define("CONFIGS_PATH", SMARTY_DIR . "/configs/");
