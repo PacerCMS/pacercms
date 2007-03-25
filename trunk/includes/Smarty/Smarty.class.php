6d734c745d72d9da98533eead1c5c394af48a4ca
stephen.yeargin <stephen.yeargin@62c4bcaa-3b1f-0410-8df8-27759b2b07fd>
2007-03-25 15:22:51 +0000

    Updates to Smarty 2.6.18, no incompatabilities found.

--- trunk/includes/Smarty/Smarty.class.php
+++ trunk/includes/Smarty/Smarty.class.php
@@ -27,10 +27,10 @@
  * @author Monte Ohrt <monte at ohrt dot com>
  * @author Andrei Zmievski <andrei@php.net>
  * @package Smarty
- * @version 2.6.15-dev
+ * @version 2.6.18
  */
 
-/* $Id: Smarty.class.php,v 1.525 2006/05/28 17:35:05 mohrt Exp $ */
+/* $Id: Smarty.class.php,v 1.528 2007/03/06 10:40:06 messju Exp $ */
 
 /**
  * DIR_SEP isn't used anymore, but third party apps might
@@ -464,7 +464,7 @@ class Smarty
      *
      * @var string
      */
-    var $_version              = '2.6.15-dev';
+    var $_version              = '2.6.18';
 
     /**
      * current template inclusion depth
