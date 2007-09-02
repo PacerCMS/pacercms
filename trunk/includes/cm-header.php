be08c6a20b471a37abf9b3e0ff7b2305238a5962
stephen.yeargin <stephen.yeargin@62c4bcaa-3b1f-0410-8df8-27759b2b07fd>
2007-09-02 01:13:46 +0000

    Slight mods to default stylesheet to remove some straggling references to original Content Manager, added robin's stylesheet for 'moonshine', still requires some template work (modified tpl files) before ready for general release

--- trunk/includes/cm-header.php
+++ trunk/includes/cm-header.php
@@ -1,7 +1,7 @@
 <?php
 
 // The Version of PacerCMS You Are Running
-define('CM_VERSION', "0.6 ALPHA");
+define('CM_VERSION', "0.6 PR1");
 
 // Make sure the config file exists
 if ( file_exists('includes/config.php') )
