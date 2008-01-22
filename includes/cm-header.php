c314ac5c864b44fabd2791fa93778040087c70cb
Stephen Yeargin <stephen@yearg.in>
2008-01-22 01:16:31 +0000

    Let's go ahead and get this one out.

--- includes/cm-header.php
+++ includes/cm-header.php
@@ -1,7 +1,7 @@
 <?php
 
 // The Version of PacerCMS You Are Running
-define('CM_VERSION', "0.6.1 PR2");
+define('CM_VERSION', "0.6.1");
 define('DB_VERSION', "65");
 
 // Make sure the config file exists
