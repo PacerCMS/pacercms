7a28f869aa8743a6f21dd87eb8099e47e4657199
stephen.yeargin <stephen.yeargin@62c4bcaa-3b1f-0410-8df8-27759b2b07fd>
2008-04-11 02:43:31 +0000

    Version bump to 0.7 PR2

--- trunk/includes/cm-header.php
+++ trunk/includes/cm-header.php
@@ -1,7 +1,7 @@
 <?php
 
 // The Version of PacerCMS You Are Running
-define('CM_VERSION', "0.7 PR1");
+define('CM_VERSION', "0.7 PR2");
 
 // Make sure the config file exists
 if ( file_exists('includes/config.php') )
